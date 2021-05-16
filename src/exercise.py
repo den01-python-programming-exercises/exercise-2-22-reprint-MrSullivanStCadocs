8a4b6b4935937e59f62913f3542c4f7920d04083
MrSullivan <>
2021-05-16 20:03:04 +0000

    variables, print, two defs, while comand

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,22 @@
 def main():
     #write your code below this line
 
+    user_number = 0
+    counter = 0
+
+    print("How many times?")
+    user_number = int(input())
+
+    while True:
+      print_text()
+      counter = counter + 1
+      if (counter == user_number):
+        break
+
+def print_text():
+    print("In a hole in the ground there lived a method")
+
+  
+
 if __name__ == '__main__':
     main()
